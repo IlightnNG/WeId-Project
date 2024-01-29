ec0c0db1a53ed5536063dee5938046d6bd557868
zhp <2778863225@qq.com>
2024-01-29 22:05:56 +0800

    Simplify ShowWeId

--- src/main/java/com/webank/weid/command/ShowWeId.java
+++ src/main/java/com/webank/weid/command/ShowWeId.java
@@ -1,31 +1,44 @@
 package com.webank.weid.command;
 
 import com.webank.weid.protocol.base.WeIdDocument;
+import com.webank.weid.protocol.base.WeIdPrivateKey;
+import com.webank.weid.protocol.base.WeIdPublicKey;
+import com.webank.weid.protocol.request.PublicKeyArgs;
+import com.webank.weid.protocol.request.SetPublicKeyArgs;
 import com.webank.weid.protocol.response.ResponseData;
 import com.webank.weid.rpc.WeIdService;
 import com.webank.weid.service.WeIdSdkService;
 import com.webank.weid.service.impl.WeIdServiceImpl;
 import com.webank.weid.util.WeIdUtils;
 import lombok.extern.slf4j.Slf4j;
+import org.fisco.bcos.web3j.crypto.ECKeyPair;
+import org.fisco.bcos.web3j.crypto.gm.GenCredential;
 
 import java.io.File;
+import java.security.KeyPair;
+import java.security.PublicKey;
 
 @Slf4j
 public class ShowWeId {
-    //private static WeIdSdkService weIdSdkService = new WeIdSdkService();
+
 
     public static void main(String[] args) {
+
+        String testWeId = "did:weid:1:0xa8c5d0b6336755882582402b24e21ebe810320da";
+
         //log提示
         log.info("[ShowWeId] begin to show WeId.");
 
         WeIdService weIdService = new WeIdServiceImpl();
+
         //show json of weid document
-        ResponseData<String> responseJson = weIdService.getWeIdDocumentJson("did:weid:1:0xa8c5d0b6336755882582402b24e21ebe810320da");
+        ResponseData<String> responseJson = weIdService.getWeIdDocumentJson(testWeId);
         System.out.println(responseJson);
         System.out.println("-----------------------------");
         //show weid document
-        ResponseData<WeIdDocument> response = weIdService.getWeIdDocument("did:weid:1:0xa8c5d0b6336755882582402b24e21ebe810320da");
-        System.out.println(response);
+        //ResponseData<WeIdDocument> response = weIdService.getWeIdDocument("did:weid:1:0xa8c5d0b6336755882582402b24e21ebe810320da");
+        //System.out.println(response);
+
 
     }
 }
