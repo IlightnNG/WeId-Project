d7ac20abed467fb273e6f2906a7432142246a64b
zhp <2778863225@qq.com>
2024-02-15 15:49:40 +0800

    Update Unix

--- tools/add_publickey.sh
+++ tools/add_publickey.sh
@@ -11,13 +11,6 @@ echo "Adding public key, please wait..."
 java ${JAVA_OPTS} -cp "$CLASSPATH" com.webank.weid.command.AddPublicKey
 
 if [ ! $? -eq 0 ]; then
-    echo "Show WeID faild, please check the log -> ../logs/error.log."
+    echo "Add public key failed, please check the log -> ../logs/error.log."
     exit $?;
-fi
-
-if [ "$1" == "no_del" ];then
-    exit 0
-fi
-if [ -f "weid" ];then
-    rm weid
 fi
